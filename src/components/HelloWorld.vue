e3650db25323a92ddcb04672edf7d26cbb7a12f0
JulioSalazar12 <u202017572@upc.edu.pe>
2022-10-16 12:52:08 -0500

    feat: base proyect

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,167 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container class="center">
+    <h1>Hola mundo</h1>
+    <v-card outlined elevation="5" class="pa-1 ma-3 center">
+      <v-container class="center">
+        <v-btn class="mx-2" color="orange" dark>
+        Guia
+        </v-btn>
+        <v-btn class="mx-2" color="error" >
+        Datos del prestamo
+        </v-btn>
+        <v-btn class="mx-2" color="primary" >
+        Datos de gastos iniciales
+        </v-btn>
+        <v-btn class="mx-2" color="green" dark >
+        Datos de gastos periodicos
+        </v-btn>
+        <v-btn class="mx-2" color="purple" dark>
+        Datos de costo de oportunidad
+        </v-btn>
+
+      </v-container>
+      </v-card>
+      <v-card outlined>
+        <v-container>
+          <v-row>
+            <v-col class="4">
+              <v-text-field
+              class="my-2"
+              dense hide-details
+              label="Outlined"
+              placeholder="Placeholder"
+              outlined
+              ></v-text-field>
+              <v-text-field
+              class="my-2"
+              dense hide-details
+              label="Outlined"
+              placeholder="Placeholder"
+              outlined
+              ></v-text-field>
+              <v-text-field
+              class="my-2"
+              dense hide-details
+              label="Outlined"
+              placeholder="Placeholder"
+              outlined
+              ></v-text-field>
+              <v-text-field
+              class="my-2"
+              dense hide-details
+              label="Outlined"
+              placeholder="Placeholder"
+              outlined
+              ></v-text-field>
+              <v-text-field
+              class="my-2"
+              dense hide-details
+              label="Outlined"
+              placeholder="Placeholder"
+              outlined
+              ></v-text-field>
+              <v-text-field
+              class="my-2"
+              dense hide-details
+              label="Outlined"
+              placeholder="Placeholder"
+              outlined
+              ></v-text-field>
+              <v-text-field
+              class="my-2"
+              dense hide-details
+              label="Outlined"
+              placeholder="Placeholder"
+              outlined
+              ></v-text-field>
+              <v-text-field
+              class="my-2"
+              dense hide-details
+              label="Outlined"
+              placeholder="Placeholder"
+              outlined
+              ></v-text-field>
+            </v-col>
+            <v-col class="4">
+              <v-text-field
+              class="my-2"
+              dense hide-details
+              label="Outlined"
+              placeholder="Placeholder"
+              outlined
+              ></v-text-field>
+              <v-text-field
+              class="my-2"
+              dense hide-details
+              label="Outlined"
+              placeholder="Placeholder"
+              outlined
+              ></v-text-field>
+              <v-text-field
+              class="my-2"
+              dense hide-details
+              label="Outlined"
+              placeholder="Placeholder"
+              outlined
+              ></v-text-field>
+              <v-text-field
+              class="my-2"
+              dense hide-details
+              label="Outlined"
+              placeholder="Placeholder"
+              outlined
+              ></v-text-field>
+              <v-text-field
+              class="my-2"
+              dense hide-details
+              label="Outlined"
+              placeholder="Placeholder"
+              outlined
+              ></v-text-field>
+            </v-col>
+            <v-col class="4">
+              <v-text-field
+              class="my-2"
+              dense hide-details
+              label="Outlined"
+              placeholder="Placeholder"
+              outlined
+              ></v-text-field>
+              <v-text-field
+              class="my-2"
+              dense hide-details
+              label="Outlined"
+              placeholder="Placeholder"
+              outlined
+              ></v-text-field>
+              <v-text-field
+              class="mt-14"
+              dense hide-details
+              label="Outlined"
+              placeholder="Placeholder"
+              outlined
+              ></v-text-field>
+              <v-text-field
+              class="my-2"
+              dense hide-details
+              label="Outlined"
+              placeholder="Placeholder"
+              outlined
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-container>
+
+      </v-card>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  export default {
+    name: 'HelloWorld',
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+    data: () => ({
+      
+    }),
+  }
+</script>
